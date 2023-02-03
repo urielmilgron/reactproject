140e623ad32c73953400adcff1affb3ca3b281e3
Uriel Milgron <urielmilgron18@gmail.com>
2023-02-03 12:56:50 -0300

    index modified and filter bar added

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import FilterBar from './FilterBar';
+import ItemNoticeContainer from './ItemNoticeContainer'
 
 const Index = () => {
     return (
         <div>
-            
+            <FilterBar/>
+            <ItemNoticeContainer/>
         </div>
     );
 }
